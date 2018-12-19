4c318766a6f61c62487f168b910ea3a3ffbced01
Alexander <meeek196@gmail.com>
2018-12-19 19:03:44 +0300

    added config front url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.use(ConfigModule).get(ConfigService);
 
-  app.use(cors(['http://localhost:4200']));
+  app.use(cors([config.get('FRONT_URL')]));
 
   await app.listen(3000);
 }
