eb6b8ba635f416149dfa48fd154388790692de84
Alexander <meeek196@gmail.com>
2019-01-10 19:44:54 +0300

    added view jwt token

--- frontend/src/app/welcome/welcome.component.ts
+++ frontend/src/app/welcome/welcome.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
 import {UserService} from '../user/user.service';
+import {TokenStorage} from '../user/token.storage';
 
 @Component({
   selector: 'app-welcome',
@@ -8,13 +10,18 @@ import {UserService} from '../user/user.service';
 })
 export class WelcomeComponent implements OnInit {
   public user;
-  constructor(private userService: UserService) { }
+  public token;
+  public show = false;
+  constructor(private userService: UserService, private readonly tokenStorage: TokenStorage) { }
 
   ngOnInit() {
     this.userService.getUser()
       .subscribe(data => {
         this.user = data;
       });
+    this.token = this.tokenStorage.token;
   }
-
+  showToken() {
+    this.show = this.show ? false : true;
+  }
 }
